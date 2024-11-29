f2063cc15403a7d68c324a6a33b7d1310c199be7
PriyanshuVijay-S <vpriyanshu@solulab.co>
2024-11-29 13:33:56 +0530

    Added comment on behaviour.py file

--- packages/valory/skills/hello_world_abci/behaviours.py
+++ packages/valory/skills/hello_world_abci/behaviours.py
@@ -269,6 +269,6 @@ class HelloWorldRoundBehaviour(AbstractRoundBehaviour):
         CollectRandomnessBehaviour,  # type: ignore
         SelectKeeperBehaviour,  # type: ignore
         PrintMessageBehaviour,  # type: ignore
-        PrintCountBehaviour,
+        PrintCountBehaviour,    # type: ignore
         ResetAndPauseBehaviour,  # type: ignore
     }
